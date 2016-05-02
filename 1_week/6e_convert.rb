6bb341b624543987a185179ac03394d008226472
Marcus <m11235w@gmail.com>
2016-05-02 20:45:30 +0100

    Convert Fahrenheit to Celsius
    This has correct examples of Wish Driven Development using ruby
    doctest.
    
    The method returns the truest and simplest thing possible for greater
    variety of use by anyone that wishes to use it.
    
    We leave the formatting to the user of the method.

--- 1_week/6e_convert.rb
+++ 1_week/6e_convert.rb
@@ -1,10 +1,33 @@
+# Write a method called convert that takes one argument which is a temperature
+# in degrees Fahrenheit. This method should return the temperature in degrees
+# Celsius.
+# doctest: convert 212F to 100C
+# >> convert(212)
+# => 100.00
+#
+# doctest: convert -40F to -40
+# >> convert(-40)
+# => -40
+#
+# doctest: convert 98.6F to 37C
+# >> convert(98.6)
+# => 37.00
+#
+# doctest: convert 98F to 36.67C
+# >> convert(98).round(6)
+# => 36.666667
+#
+# To format the output to say 2 decimal places, we can use the Kernel's format
+# method. For example, if x = 45.5678 then format("%.2f", x) will return the
+# string 45.57. Another way is to use the round function as follows:
+
 # Celsius = Fahrenheit * 9 / 5 + 32
 # 100 degrees C = 212 degrees F
 
-def f_to_c(f)
-  c = (f - 32) * 5 / 9
-  puts "The temperature in Celsius is #{c}"
-  puts "to 2 decimal places"
+def convert(f)
+  (f.to_f - 32) * 5/9
 end
 
-f_to_c(212)
+if __FILE__ == $PROGRAM_NAME
+  puts format('%.2f', convert(-40))
+end
