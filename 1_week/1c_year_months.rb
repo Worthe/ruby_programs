bf8d84369d7e1704be1ea4b087a1875f4b468db5
Marcus <m11235w@gmail.com>
2016-05-07 19:40:25 +0100

    Apparently we had the wrong content in a file

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -1,1 +1,20 @@
-
+# Using what we have learned so far: I wonder if anyone could have a crack at
+# working out the age in years and months form. For example “I’m 32 years and 6
+# months old.” Use the following values for age_in_seconds:`
+# doctest: age in seconds converted to months
+# >> seconds_to_months(315_360_00)
+# => 12.00
+#
+# 979000000
+#
+# 2158493738
+#
+# 246144023
+#
+# 1270166272
+#
+# 102560009
+#
+def seconds_to_months(seconds)
+  puts 23
+end
--- 1_week/3e_how_old.rb
+++ 1_week/3e_how_old.rb
@@ -1,2 +1,34 @@
-age_in_secs = 154483200 # 44.6 years
+# Write a Ruby program that displays how old I am, if I am 979000000 seconds
+# old.
+# doctest: how_old_in_seconds
+# >> how_old_in_seconds(979000000)
+# => 979000000.00
+#
+# Display the result as a floating point (decimal) number to two decimal
+# places (for example, 17.23).
+#
+# Note: To format the output to say 2 decimal places, we can use the Kernel's
+# format method. For example, if x = 45.5678 then format("%.2f", x) will return
+# the string 45.57
+
+def how_old_in_seconds(979000000)
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# age_in_secs = 154483200 # 44.6 years
 puts "my age in seconds is #{format("%.2f", age_in_secs)}"
